c450f1f41cbe0512ec91ee3cba0589000cc2bb1e
Thirumoorthi <thirumoorthi30042000@gmail.com>
2025-10-14 22:07:04 +0530

    profile card project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <header className="bg-green-600 text-white py-4 shadow-md">
       <h1 className="text-2xl font-bold text-center tracking-wide">
-        TO-DO List
+        PROJECTS
       </h1>
     </header>
   );
