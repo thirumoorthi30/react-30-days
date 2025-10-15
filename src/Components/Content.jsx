3cd2cdb1541057b2ca387f4b127610d537889531
Thirumoorthi <thirumoorthi30042000@gmail.com>
2025-10-15 11:56:00 +0530

    changes for build

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,16 +2,29 @@ import React from "react";
 
 const Content = () => {
 
-  const printSomething = () => {
+  let user = "testing";
+
+  const printSomething = (e) => {
+    console.log(e.target.innerText);
     console.log("testing clicked");
+    user = "Thiru";
+    console.log(user);
   }
+
+  const printSomethingOne = (event) => {
+    console.log(event.target.innerText);
+    console.log("Hello world");
+    user = "Thirumoorthi";
+    console.log(user);
+  }
+
   return (
     <main className="flex flex-col items-center justify-center p-8 bg-gray-100 min-h-[60vh]">
       <h1 className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-lg text-xl font-semibold">
-        Content
+        Content - {user}
       </h1>
       <button onClick={printSomething}>Click</button>
-      <button onClick={printSomething}>Click</button>
+      <button onClick={ (e) => {printSomethingOne(e)}}>Click me 1</button>
     </main>
 
   );
